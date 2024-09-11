aed9d12fcf02fd36cf9d9e2de87b4991f8dfc827
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-09-11 01:38:51 -0300

    Added custom template function "humanDate";

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -21,10 +21,10 @@ func (app *application) serverError(w http.ResponseWriter, r *http.Request, err 
 // The clientError helper sends a specific status code and corresponding description
 // to the user. We'll use this later in the book to send responses like 400 "Bad
 // Request" when there's a problem with the request that the user sent.
-
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
+
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
 	// Retrieve the appropriate template set from the cache based on the page
